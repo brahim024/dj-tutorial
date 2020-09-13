b3d72d10f53bdb3b720288467869b82fba383738
brahim024 <ifninos168@gmail.com>
2020-09-13 20:20:09 +0200

    create class with templateview

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from myapp.views import PublisherList , PublisherDetail, AuthorList
+from django.views.generic import TemplateView
+from myapp.views import AuthorView
+
+
 urlpatterns = [
-    path('publisher/',PublisherList.as_view()),
-    path('publisher/<int:id>/',PublisherDetail.as_view()),
-    path('auther/',AuthorList.as_view()),
+    #path('publisher/',PublisherList.as_view()),
+    #path('publisher/<int:id>/',PublisherDetail.as_view()),
+    path('ex1',TemplateView.as_view(template_name='author1.html',extra_context={'title':'Custom Title'})),
+    path('ex2',AuthorView.as_view(),name='ex2'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,11 @@
-from django.views.generic import DetailView ,ListView
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
+#from django.shortcuts import render
+#from django.views import View
+#from django.http import HttpResponseForbidden,HttpResponsePermanentRedire
+# from django .urls import reverse
+from myapp.models import Author
 
-from myapp.models import Book, Publisher ,Author
-
-class PublisherList(ListView):
+'''class PublisherList(ListView):
 	model= Publisher
 	context_object_name='my_favorite_publisher'
 class PublisherDetail(DetailView):
@@ -12,10 +14,27 @@ class PublisherDetail(DetailView):
 	def get_context_data(self, **kwargs):
 		# call the base implimentation first to get context
 		context=super().get_context_data(**kwargs)
-		#add in queryset of all the book
+	 	#add in queryset of all the book
 		context['book_list']=Book.objects.all()
 		return context
 		
 class AuthorList(ListView):
 	model=Author
 	context_object_name='author'
+
+
+class RecordInterest(SingleObjectMixin, View):
+	models=Author
+	def post (self,request ,*args, **kwargs):
+		if not request.user.is_authenticated:
+			return HttpResponseForbidden()
+		self.object=self.get_object() #here we search for authers
+		return HttpResponsePermanentRedirect(reverse('author-detail',kwargs={'pk':self.object.pk}))
+''' 
+class AuthorView(TemplateView):
+	template_name='myapp/author2.html'
+	def get_context_data(self,**kwargs):
+		context=super().get_context_data(**kwargs)
+		context['authors']=Author.objects.get(id=1)
+		context['data']="Context Data for Author"
+		return context
