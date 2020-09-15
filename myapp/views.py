7e0b9b1993a542ff6f28af63497020d86851e0f2
brahim024 <ifninos168@gmail.com>
2020-09-15 21:57:07 +0200

    add pagination with function based views

--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,28 @@
 #from django.views.generic.base import TemplateView
 from django.shortcuts import render ,get_object_or_404
+from django.core.paginator import Paginator, EmptyPage,\
+												PageNotAnInteger
 #from django.views import View
 #from django.http import HttpResponseForbidden,HttpResponsePermanentRedire
 # from django .urls import reverse
 from myapp.models import Post
 def post_list(request):
-	posts=Post.objects.all()
+	object_list=Post.objects.all()
+	paginator=Paginator(object_list,2) #her we want 2 objects(post)
+	page=request.GET.get('page')
+	try:
+		posts=paginator.page(page)
+	except PageNotAnInteger:
+		#if oage in not integer delivred the first page
+		posts=paginator.page(1)
+	except EmptyPage:
+		#if page has out of range
+		posts=paginator.page(paginator.num_pages)
 	return render(request,'post_list.html',{'posts':posts})
+
+
+
+
 def post_details(request,year,month,day ,post):
 	post=get_object_or_404(Post,slug=post,
 								status='published',
