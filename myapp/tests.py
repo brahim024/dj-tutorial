3a72abbf529d02d4633756f3a6b870718e0c6358
brahim024 <ifninos168@gmail.com>
2020-10-17 16:41:19 +0200

    add models testing

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
-
+from myapp.models import Post
 # Create your tests here.
+class PostTestcase(TestCase):
+	def setUp(self):
+		Post.objects.all()
+	def test_post(self):
+		post=Post.object.get(tag_slug)
+		self.assertEqual(tag_slug)
+		
